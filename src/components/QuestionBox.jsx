39ec0f9ae87ed40d5900c4979689e8408bb88cdc
patilarya76 <patilaryaanil07@gmail.com>
2024-01-19 17:57:27 +0530

    done with some functinality

--- src/components/QuestionBox.jsx
+++ src/components/QuestionBox.jsx
@@ -1,27 +1,72 @@
-import React from 'react'
-import './QuestionBox.css'; 
+// src/components/QuestionBox.jsx
+
+import React, { useState } from 'react';
+import './QuestionBox.css';
+import questionsData from '../questions.jsx'; 
 
 export default function QuestionBox() {
-  return (
-    <div>
+  const [isWhiteMode, setIsWhiteMode] = useState(true);
+  const [highlightedQuestion, setHighlightedQuestion] = useState(false);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const toggleMode = () => {
+    setIsWhiteMode((prevMode) => !prevMode);
+  };
+
+  const highlightQuestion = () => {
+    setHighlightedQuestion(true);
+  };
+
+  const removeHighlight = () => {
+    setHighlightedQuestion(false);
+  };
+
+  const handleOptionClick = () => {
+   
+    if (currentQuestionIndex < questionsData.length - 1) {
+      setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
       
-    <button className='darkmode'>Dark Mode</button>
-    <h1>Let's improve our General Knowledge</h1>
-    {/* <button className='lightmode'></button> */}
-    <div className='Questionsmain'>
-      <p>Question 1 out of 5</p>
-      <p className='question1'>Which planet is known as red planet?</p>
-      <p className='options1'> a)Venus b)Mars <br />
-       c)Jupiter  d)Saturn</p>
-       {/* <p className='question2'>What is the capital city of Japan?</p>
-       <p className='options2'>a)Seoul     b)  Beijing  <br />
-           c) Tokyo     d) Bangkok </p>
-           <p className='question2'> Who wrote the play "Romeo and Juliet"?</p>
-       <p className='options2'>a)Seoul     b)  Beijing  <br />
-           c) Tokyo     d) Bangkok </p> */}
-      <button className='highlight'> Highlight</button>
-      <button className='removehighlight'> Remove Highlight</button>
-    </div>
-    </div>
-  )
+      setHighlightedQuestion(false);
+    }
+  };
+
+  const currentQuestion = questionsData[currentQuestionIndex];
+
+  return (
+    <>
+      <div className={`Main ${isWhiteMode ? 'white-mode' : 'dark-mode'}`}>
+        <div>
+          <button className='changemode' onClick={toggleMode}>
+            {isWhiteMode ? 'Dark Mode' : 'White Mode'}
+          </button>
+        </div>
+        <div className='text'>
+          Here's a chance to have fun!!
+        </div>
+
+        <div className="questions">
+          <span className={`question-text ${highlightedQuestion ? 'highlighted' : ''}`}>
+            {currentQuestion.text}
+          </span>
+        </div>
+        <p></p>
+
+        <div className="options">
+          {currentQuestion.options.map((option) => (
+            <button key={option.id} className='option' onClick={handleOptionClick}>
+              {option.text}
+            </button>
+          ))}
+        </div>
+        <div className="buttons">
+          <button id='previous' className='button' onClick={highlightQuestion}>
+            Highlights
+          </button>
+          <button id='remove' className='button' onClick={removeHighlight}>
+            Remove Highlights
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
